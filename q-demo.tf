df0f0493c9dfc358aff6ffec7a77acad6cb661d8
Chrozwel <166760473+Chrozwel@users.noreply.github.com>
2025-04-19 16:37:56 +0800

    Add files via upload

---
+++ q-demo.tf
@@ -0,0 +1,17 @@
+resource "aws_sqs_queue" "our_first_mailbox" {
+    name = "csse6400_prac"
+}
+
+resource "aws_sqs_queue" "our_first_fifo" {
+    name = "csse6400_prac.fifo"
+    fifo_queue = true
+    content_based_deduplication = true
+}
+
+output "mailbox" {
+    value = aws_sqs_queue.our_first_mailbox.arn
+}
+
+output "fifo" {
+    value = aws_sqs_queue.our_first_fifo.arn
+}
